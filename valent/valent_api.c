b40b049fece86a28fc15c44da97518cd0243bd05
dlandahl <36662196+dlandahl@users.noreply.github.com>
2023-12-16 12:44:10 +0000

    valent

---
+++ valent/valent_api.c
@@ -0,0 +1,42 @@
+
+#include "valent.h"
+
+struct Vga_Format__Zones {
+    vdl_constant_t video;
+    vdl_constant_t front;
+    vdl_constant_t sync;
+    vdl_constant_t back;
+};
+
+struct Vga_Format {
+    struct Vga_Format__Zones horizontal;
+    struct Vga_Format__Zones vertical;
+};
+
+int main() {
+    vdl_t workspace = vdl_create_workspace("My computer chip");
+
+    const char *code = read_entire_file("syntax.my_hdl");
+
+    vdl_add_source_code(workspace, code);
+    vdl_compile(workspace);
+
+    int timestep = 1; // ps
+
+    vdl_sim_t *simulation = vdl_create_simulation(workspace, timestep);
+
+    struct Vga_Format vga_format = {
+        { 480, 11, 2,  31 },
+        { 640, 16, 96, 48 }
+    };
+
+    vdl_instantiate_object(simulation, "Vga_Generator", "/vga_gen", &vga_format, vdl_lookup(my_circuit));
+
+    for (int i = 0; i < 100; i++) {
+        vdl_run_simulation(simulation, 5000);
+
+        vdl_object_t *clock = vdl_get_object(simulation, "/vga_gen/in.clk");
+    }
+
+    const char *verilog = vdl_generate_code(ast, MHDL_CODE_VERILOG);
+}
