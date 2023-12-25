55c70fa6f61357f5bd23868e27fa970b01622282
dlandahl <36662196+dlandahl@users.noreply.github.com>
2023-12-25 11:53:21 +0000

    Type checking

--- valent/valent_api.c
+++ valent/valent_api.c
@@ -2,10 +2,10 @@
 #include "valent.h"
 
 struct Vga_Format__Zones {
-    vdl_constant_t video;
-    vdl_constant_t front;
-    vdl_constant_t sync;
-    vdl_constant_t back;
+    vlnt_constant_t video;
+    vlnt_constant_t front;
+    vlnt_constant_t sync;
+    vlnt_constant_t back;
 };
 
 struct Vga_Format {
@@ -14,31 +14,37 @@ struct Vga_Format {
 };
 
 int main() {
-    vdl_t workspace = vdl_create_workspace("My computer chip");
+    vlnt_t workspace = vlnt_create_workspace("My computer chip");
 
-    const char *code = read_entire_file("syntax.my_hdl");
+    const char *code = read_entire_file("syntax.valent");
 
-    vdl_add_source_code(workspace, code);
-    vdl_compile(workspace);
-
-    int timestep = 1; // ps
-
-    vdl_sim_t *simulation = vdl_create_simulation(workspace, timestep);
+    vlnt_add_source_code(workspace, code);
 
     struct Vga_Format vga_format = {
         { 480, 11, 2,  31 },
         { 640, 16, 96, 48 }
     };
 
-    vdl_instantiate_object(simulation, "Vga_Generator", "/vga_gen", &vga_format, vdl_lookup(my_circuit));
-    vdl_object_t *clock = vdl_get_object(simulation, "/vga_gen/in.clk");
+    vlnt_sim_options_t options;
+    options.timestep_ps = 1;
+    options.top = "Vga_Generator";
+    options.top_params = { &vga_format };
 
-    for (int i = 0; i < 100000; i++) {
-        vdl_step_simulation(simulation);
+    vlnt_sim_t *simulation = vlnt_create_simulation(workspace, options);
 
-        if (i % 5000 == 0)
-            vdl_logic_not(clock);
+    const char *binary = read_entire_file("Image.bin");
+    vlnt_load_memory(simulation, "bram_vmem.memory", binary);
+
+    for (int i = 0; i < 10; i++) {
+        vlnt_run_for_ns(simulation, 5);
+
+        vlnt_set("in.clk", vlnt_op_bitnot(vlnt_get("in.clk"));
     }
 
-    const char *verilog = vdl_generate_code(ast, VDL_CODE_VERILOG);
+    vlnt_compile_options_t options;
+    options.top = "Vga_Generator";
+    vlnt_compile(workspace);
+
+    const char *verilog = vlnt_generate_code(workspace, VLNT_CODE_VERILOG);
+    const char *rtlil = vlnt_generate_code(workspace, VLNT_CODE_RTLIL);
 }
