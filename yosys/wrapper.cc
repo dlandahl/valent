d498a8275a8a1ec565bfeb69765c566898d48fae
dlandahl <36662196+dlandahl@users.noreply.github.com>
2024-01-03 14:56:55 +0000

    RTLIL

--- yosys/wrapper.cc
+++ yosys/wrapper.cc
@@ -6,7 +6,8 @@ using namespace Yosys;
 
 typedef void RTLIL_Module;
 typedef void RTLIL_Wire;
-typedef struct { uint8_t payload[sizeof(RTLIL::SigSpec)]; } RTLIL_Signal;
+typedef void RTLIL_Process;
+typedef void RTLIL_Signal;
 
 extern "C" {
     __declspec(dllexport) void rtlil_initialise_design() {
@@ -24,7 +25,7 @@ extern "C" {
         run_pass(command);
     }
 
-    __declspec(dllexport) RTLIL_Module *rtlil_create_module(const char *name) {
+    __declspec(dllexport) RTLIL_Module *rtlil_add_module(const char *name) {
 
         RTLIL::Module *module = new RTLIL::Module;
         module->name = std::string(name);
@@ -32,8 +33,9 @@ extern "C" {
         return module;
     }
 
-    __declspec(dllexport) void finish_module(RTLIL_Module *module) {
+    __declspec(dllexport) void rtlil_finish_module(RTLIL_Module *module) {
         RTLIL::Module *_module = (RTLIL::Module *)module;
+        _module->fixup_ports();
         yosys_design->add(_module);
     }
 
@@ -50,26 +52,74 @@ extern "C" {
         return wire;
     }
 
-    __declspec(dllexport) RTLIL_Signal rtlil_signal_from_wire(RTLIL_Wire *wire) {
+    __declspec(dllexport) RTLIL_Signal *rtlil_signal_from_wire(RTLIL_Wire *wire) {
 
         RTLIL::Wire *_wire = (RTLIL::Wire *)wire;
-        RTLIL::SigSpec sig(_wire);
-        return *(RTLIL_Signal *)&sig;
+        RTLIL::SigSpec* result = new RTLIL::SigSpec(_wire);
+        return (RTLIL_Signal*)result;
     }
 
-    __declspec(dllexport) RTLIL_Signal rtlil_signal_from_constant(int value, int width) {
+    __declspec(dllexport) RTLIL_Signal *rtlil_signal_from_constant(int value, int width) {
 
         RTLIL::Const _const(value, width);
-        RTLIL::SigSpec sig(_const);
-        return *(RTLIL_Signal *)&sig;
+        RTLIL::SigSpec* result = new RTLIL::SigSpec(_const);
+        return (RTLIL_Signal*)result;
     }
 
-    __declspec(dllexport) void rtlil_connect_signals(RTLIL_Module *module, RTLIL_Signal left, RTLIL_Signal right) {
+    __declspec(dllexport) void rtlil_connect_signals(RTLIL_Module *module, RTLIL_Signal *left, RTLIL_Signal *right) {
 
         RTLIL::Module *_module =  (RTLIL::Module *)module;
-        RTLIL::SigSpec _left   = *(RTLIL::SigSpec *)&left;
-        RTLIL::SigSpec _right  = *(RTLIL::SigSpec *)&right;
+        RTLIL::SigSpec _right  = *(RTLIL::SigSpec *)right;
+        RTLIL::SigSpec _left   = *(RTLIL::SigSpec *)left;
 
         _module->connect(RTLIL::SigSig(_left, _right));
+    }
+
+    __declspec(dllexport) RTLIL_Signal *rtlil_add_binary_cell(RTLIL_Module *module, const char *type, RTLIL_Signal *left, RTLIL_Signal *right, int width, int line_number) {
+        RTLIL::Module *_module = (RTLIL::Module *)module;
+
+        IdString name = stringf("%s$%s:%d$%d", type, "_VALENT_", line_number, autoidx++);
+        RTLIL::Cell* cell = _module->addCell(name, type);
+
+        cell->parameters[ID::A_SIGNED] = RTLIL::Const(0);
+        cell->parameters[ID::B_SIGNED] = RTLIL::Const(0);
+
+        RTLIL::SigSpec _left = *(RTLIL::SigSpec *)left, _right = *(RTLIL::SigSpec *)right;
+        cell->parameters[ID::A_WIDTH] = RTLIL::Const(_left.size());
+        cell->parameters[ID::B_WIDTH] = RTLIL::Const(_right.size());
+
+        cell->setPort(ID::A, _left);
+        cell->setPort(ID::B, _right);
+
+        cell->parameters[ID::Y_WIDTH] = width;
+    
+        RTLIL::Wire *wire = _module->addWire(cell->name.str() + "_Y", width);
+        cell->setPort(ID::Y, wire);
+
+        RTLIL::SigSpec *result = new RTLIL::SigSpec(wire);
+        return (RTLIL_Signal *)result;
+    }
+
+    __declspec(dllexport) RTLIL_Process rtlil_add_process(RTLIL_Module *module, const char *name, bool sequential, RTLIL_Signal *sync) {
+        RTLIL::Module *_module = (RTLIL::Module *)module;
+        RTLIL::Process *proc = _module->addProcess(stringf("$proc$%s", name));
+        RTLIL::SyncRule *syncrule = new RTLIL::SyncRule;
+
+        syncrule->type = sequential ? RTLIL::STp : RTLIL::STa;
+
+        RTLIL::SigSpec _sync = *(RTLIL::SigSpec *)sync;
+        if (sequential) {
+            syncrule->signal = _sync;
+        }
+        proc->syncs.push_back(syncrule);
+    }
+
+    __declspec(dllexport) RTLIL_Signal *rtlil_signal_by_name(RTLIL_Module *module, const char *name) {
+        RTLIL::Module *_module = (RTLIL::Module *)module;
+        IdString id = std::string(name);
+        Wire *wire = _module->wires_.at(id);
+
+        RTLIL::SigSpec* result = new RTLIL::SigSpec(wire);
+        return (RTLIL_Signal*)result;
     }
 }
