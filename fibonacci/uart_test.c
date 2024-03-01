b7c48224c875f1228d601ea358560453ceba742f
dlandahl <landahl2014@gmail.com>
2024-03-01 15:39:06 +0000

    Fix UART MMIO

--- fibonacci/uart_test.c
+++ fibonacci/uart_test.c
@@ -2,35 +2,35 @@
 #include "stdint.h"
 
 enum uart_flags {
-  READY    = 1,
-  EXECUTE  = 2,
-};
-
-struct uart {
-  uint64_t byte;
-  union {
-    enum uart_flags flags;
-    uint64_t pad;
-  };
+  UART_READY    = 1 << 8,
+  UART_EXECUTE  = 1 << 9,
 };
 
 volatile uint64_t* const MMIO_SWITCHES = 0x100;
 volatile uint64_t* const MMIO_LEDS     = 0x108;
-volatile struct uart* const MMIO_UART  = 0x110;
-volatile uint64_t* const MMIO_BUTTONS  = 0x120;
+volatile uint64_t* const MMIO_UART     = 0x110;
+volatile uint64_t* const MMIO_BUTTONS  = 0x118;
+
+const uint8_t* message = "Hello, Sailor!\n";
 
 int main(void) {
+  uint64_t leds;
+
   while (1) {
-    if (!*MMIO_BUTTONS) continue;
-    uint8_t byte = *MMIO_SWITCHES;
+    leds += 1;
+    *MMIO_LEDS = leds;
 
-    enum uart_flags flags = MMIO_UART->flags;
-    while (!(flags & READY)) {
-      flags = MMIO_UART->flags;
+    for (int i = 0; i < 15; i++) {
+      uint64_t byte = message[i];
+
+      enum uart_flags flags = *MMIO_UART;
+      while (!(flags & UART_READY)) {
+        flags = *MMIO_UART;
+      }
+
+      *MMIO_UART = byte | UART_EXECUTE;
+      *MMIO_LEDS = byte;
     }
-
-    MMIO_UART->byte = byte;
-    MMIO_UART->flags = EXECUTE;
   }
 
   return 0;
